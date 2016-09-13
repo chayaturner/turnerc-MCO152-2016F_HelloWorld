3a025dd9fb5db1b20ff5858718b03e6bd43b9280
Chaya Turner <dietaholic@gmail.com>
2016-09-13 19:58:50 -0400

    edited program

--- src/helloworld/Program.java
+++ src/helloworld/Program.java
@@ -3,8 +3,8 @@ package helloworld;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("This is being pushed to Github!");
 	}
 
 }
