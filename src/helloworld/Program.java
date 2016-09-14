09ebc1c9eb3bbc876c0ba75a571536c2bf0e807a
Chaya Turner <chaya.turner@student.touro.edu>
2016-09-13 20:01:12 -0400

    added comment

--- src/helloworld/Program.java
+++ src/helloworld/Program.java
@@ -1,7 +1,8 @@
 package helloworld;
 
 public class Program {
-
+	
+	//git practice program
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("This is being pushed to Github!");
